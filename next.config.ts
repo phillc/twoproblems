2fda93b03e75a78362bb6b507a695e316e78a243
phillc <15082+phillc@users.noreply.github.com>
2025-08-29 08:59:37 -0400

    Attempt to convert to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const basePath = process.env.BASE_PATH || '';
+
+const nextConfig: NextConfig = {
   output: 'export',
+  basePath: basePath || undefined,
   images: {
     unoptimized: true
   }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
